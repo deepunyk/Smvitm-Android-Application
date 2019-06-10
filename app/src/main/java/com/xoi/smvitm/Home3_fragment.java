da90c02592b95de51ddc171f6bf96de790a92019
Deepak Nayak <39850063+deepunyk@users.noreply.github.com>
2019-06-10 21:30:29 +0530

    1.0

--- app/src/main/java/com/xoi/smvitm/Home3_fragment.java
+++ app/src/main/java/com/xoi/smvitm/Home3_fragment.java
@@ -1,5 +1,8 @@
 package com.xoi.smvitm;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,48 +12,117 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class Home3_fragment extends Fragment {
 
     private ArrayList<String> descriptions = new ArrayList<>();
+    private ArrayList<String> links = new ArrayList<>();
+    private ArrayList<String> dates = new ArrayList<>();
     View view;
+    ProgressDialog loading;
+    SharedPreferences sharedPreferences;
+    Button refresh;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_3,container,false);
-        initImageBitmaps();
+        refresh = (Button)view.findViewById(R.id.btnRefresh);
+
+        sharedPreferences = getActivity().getSharedPreferences("com.xoi.smvitm", Context.MODE_PRIVATE);
+        try {
+            descriptions = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("Circular description", ObjectSerializer.serialize(new ArrayList<String>())));
+            links = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("Circular link", ObjectSerializer.serialize(new ArrayList<String>())));
+            dates = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("Circular dates", ObjectSerializer.serialize(new ArrayList<String>())));
+            initRecyclerView();
+        }
+        catch (Exception e){
+            refresh.performClick();
+        }
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                descriptions.removeAll(descriptions);
+                links.removeAll(links);
+                dates.removeAll(dates);
+                getCirculars();
+            }
+        });
         return view;
     }
-    private void initImageBitmaps(){
 
-        descriptions.add("Havasu Falls");
+    private void getCirculars() {
+        loading =  ProgressDialog.show(getActivity(),"Updating the list","please wait",false,true);
 
-        descriptions.add("Trondheim");
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://script.google.com/macros/s/AKfycbxIpWEw0QJ5FEc_106HZqOS6DD2QKpRdoZ1nLmUxDyda-v8M-g/exec?action=getCirculars",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        parseItems(response);
+                    }
+                },
 
-        descriptions.add("Portugal");
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
-        descriptions.add("Rocky Mountain National Park");
+                    }
+                }
+        );
+        int socketTimeOut = 50000;
+        RetryPolicy policy = new DefaultRetryPolicy(socketTimeOut, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT);
+        stringRequest.setRetryPolicy(policy);
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        queue.add(stringRequest);
+    }
 
-        descriptions.add("Mahahual");
+    private void parseItems(String jsonResposnce) {
+        try {
+            JSONObject jobj = new JSONObject(jsonResposnce);
+            JSONArray jarray = jobj.getJSONArray("circulars");
 
-        descriptions.add("Frozen Lake");
-
-        descriptions.add("White Sands Desert");
-
-        descriptions.add("Austrailia");
-
-        descriptions.add("Washington");
-
-        initRecyclerView();
+            for (int i = 0; i < jarray.length(); i++) {
+                JSONObject jo = jarray.getJSONObject(i);
+                String description_json = jo.getString("description");
+                descriptions.add(description_json);
+                String links_json = jo.getString("link");
+                links.add(links_json);
+                String dates_json = jo.getString("date");
+                dates.add(dates_json);
+            }try {
+                sharedPreferences.edit().putString("Circular description", ObjectSerializer.serialize(descriptions)).apply();
+                sharedPreferences.edit().putString("Circular link", ObjectSerializer.serialize(links)).apply();
+                sharedPreferences.edit().putString("Circular dates", ObjectSerializer.serialize(dates)).apply();
+            }
+            catch (Exception e) {
+            }
+            initRecyclerView();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     private void initRecyclerView(){
         RecyclerView recyclerView = view.findViewById(R.id.recyclerview);
-        Recycler_View_Adapter adapter = new Recycler_View_Adapter(descriptions,getActivity());
+        Recycler_View_Adapter adapter = new Recycler_View_Adapter(descriptions,links,dates,getActivity());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        loading.dismiss();
     }
 }
--- app/src/main/java/com/xoi/smvitm/Recycler_View_Adapter.java
+++ app/src/main/java/com/xoi/smvitm/Recycler_View_Adapter.java
@@ -1,12 +1,15 @@
 package com.xoi.smvitm;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,12 +18,18 @@ import java.util.ArrayList;
 public class Recycler_View_Adapter extends RecyclerView.Adapter<Recycler_View_Adapter.ViewHolder>{
 
     private ArrayList<String> descriptions = new ArrayList<String>();
+    private ArrayList<String> links = new ArrayList<String>();
+    private ArrayList<String> dates = new ArrayList<String>();
     private Context mContext;
 
-    public Recycler_View_Adapter(ArrayList<String> descriptions, Context mContext) {
+    public Recycler_View_Adapter(ArrayList<String> descriptions, ArrayList<String> links, ArrayList<String> dates, Context mContext) {
         this.descriptions = descriptions;
+        this.links = links;
+        this.dates = dates;
         this.mContext = mContext;
     }
+
+
 
     @NonNull
     @Override
@@ -33,10 +42,18 @@ public class Recycler_View_Adapter extends RecyclerView.Adapter<Recycler_View_Ad
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
         viewHolder.description.setText(descriptions.get(i));
+        viewHolder.date.setText(dates.get(i));
         viewHolder.parent_layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(mContext, descriptions.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewHolder.downloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(links.get(i));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                mContext.startActivity(intent);
             }
         });
     }
@@ -48,14 +65,16 @@ public class Recycler_View_Adapter extends RecyclerView.Adapter<Recycler_View_Ad
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        TextView description;
+        TextView description, date;
         ConstraintLayout parent_layout;
+        Button downloadBtn;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             description = itemView.findViewById(R.id.description);
+            date = itemView.findViewById(R.id.date);
             parent_layout = itemView.findViewById(R.id.parent_layout);
-
+            downloadBtn = itemView.findViewById(R.id.downloadBtn);
         }
     }
 }
